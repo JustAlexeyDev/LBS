f8c15e93d50b8c3590aac360dc840417b7c64680
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-04 22:49:35 +0900

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import Header from "./Components/Header";
+const App = () => {
+  return(
+    <div>
+      <Header/>
+      <p>леха и игорь канон</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Illo illum perferendis incidunt minima sint maiores id? Porro consectetur dignissimos hic atque, aut nesciunt officiis quisquam alias laborum vero doloremque pariatur?</p>
+    </div>
+  );
+}
+export default App;
