80e5ec130bc665f3afda477e65bc58f8f2c831e1
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-05 01:55:17 +0900

    Update Header.jsx

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,22 +1,37 @@
-import {CircleUser, Bell} from "lucide-react"
+import { CircleUser, Bell } from "lucide-react";
+import React, { useEffect, useState } from "react";
+import { useNavigate } from "react-router";
 
 const Header = () => {
-  return(
-    <div className='Header--Container'>
+  const [pageName, setPageName] = useState("Ошибка");
+  const navigate = useNavigate();
 
-      <button className='kinich'>
-        <CircleUser color="#fff"/>
+  useEffect(() => {
+    const path = window.location.pathname;
+    if (path === "/" || path === "/Home") {
+      setPageName("Главная");
+    } else if (path === "/Profile") {
+      setPageName("Профиль");
+    } else if (path === "/Notifications") {
+      setPageName("Уведомления");
+    }
+  }, [location.pathname]);
+
+  return (
+    <div className='Header--Container'>
+      <button className='header-button' onClick={() => navigate("/Profile")}>
+        <CircleUser color="#fff" />
       </button>
 
-      <div className='kinich'>
-        <p>Главная</p>
+      <div className='header-title'>
+        <p>{pageName}</p>
       </div>
 
-      <button className='kinich'>
-        <Bell  color="#fff" />
+      <button className='header-button' onClick={() => navigate("/Notifications")}>
+        <Bell color="#fff" />
       </button>
-
     </div>
   );
 }
+
 export default Header;
