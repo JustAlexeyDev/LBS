495800de934a312f94909e0fe5cdab9efa48c0da
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-05 01:41:04 +0900

    Update index.jsx

--- src/index.jsx
+++ src/index.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+
+import { BrowserRouter } from "react-router-dom";
 import "./Assets/Css/Global.css";
 import "./Assets/Css/Reset.css";
 
@@ -8,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
