2a594d35469e99b071b3ac85c1fbf82f7a6950ad
Just Alex <119042027+JustAlexeyDev@users.noreply.github.com>
2024-10-04 22:49:29 +0900

    Create Header.jsx

---
+++ src/Components/Header.jsx
@@ -0,0 +1,16 @@
+const Header = () => {
+  return(
+    <div className='Header--Container'>
+      <button className='kinich'>
+
+      </button>
+      <div className='kinich'>
+        <p>Главная</p>
+      </div>
+      <button className='kinich'>
+
+      </button>
+    </div>
+  );
+}
+export default Header;
